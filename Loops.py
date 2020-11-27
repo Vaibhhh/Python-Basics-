011351e706c20ef6c84bf44eb857a61914f6fd7e
Vaibhav Joshi <67133735+Vaibhhh@users.noreply.github.com>
2020-11-28 03:11:03 +0530

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,16 @@
+#While Loop
+count = 0
+while count<9:
+  print("Number:",count)
+  count = count+1
+
+print("Good Bye")  
+
+#For Loop 
+fruits = ['Mango','Grapes','Apple']
+
+for fruit in fruits:
+  print("current fruits:",fruit)
+
+print("Good bye")
+
