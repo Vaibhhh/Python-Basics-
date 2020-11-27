99e4af611ee2bbe22eb255c34f238098aaeeaad7
Vaibhav Joshi <67133735+Vaibhhh@users.noreply.github.com>
2020-11-28 01:29:22 +0530

    Create Nested Dictionary.py

---
+++ Nested Dictionary.py
@@ -0,0 +1,5 @@
+#Nested Array
+emp_details={'Employee':{'Dave':{'ID':"001",'Salary':'2000',"Designation":"Team Lead"},
+"Ava":{'ID':'002',"Salary":'1000',"Designation":"Associate"}}}
+
+print(emp_details)
