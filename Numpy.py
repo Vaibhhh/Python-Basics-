0ca9bce635562645d2e4876d0b4db7817fe70c0e
Vaibhav Joshi <67133735+Vaibhhh@users.noreply.github.com>
2020-11-30 01:46:18 +0530

    Create Numpy.py

---
+++ Numpy.py
@@ -0,0 +1,24 @@
+#Numpy
+import numpy as np
+
+a=np.array([(1,2,3),(4,5,6)])
+
+print(a)
+
+import time
+
+SIZE = 1000
+L1 = range(SIZE)
+L2 = range(SIZE)
+
+A1 = np.arange(SIZE)
+A2 = np.arange(SIZE)
+
+start = time.time()
+
+result = [(x,y) for x,y in zip(L1,L2)]
+
+start = time.time()
+result = A1*A2
+
+print((time.time()-start)*1000)
